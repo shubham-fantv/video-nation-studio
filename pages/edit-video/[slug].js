f7a54ffecd7aed0f1f2bc424925459b544de763e
shubham-ft <139536877+shubham-fantv@users.noreply.github.com>
2025-04-21 08:08:46 +0530

    iframe url updated

--- pages/edit-video/[slug].js
+++ pages/edit-video/[slug].js
@@ -1,18 +1,54 @@
-import React from "react";
+// import React from "react";
+// import { useRouter } from "next/router";
+
+// const Edit = () => {
+//   const router = useRouter();
+//   const { slug } = router.query;
+//   console.log(slug);
+//   return (
+//     <div className="h-screen w-screen">
+//       <iframe
+//         src={`http://localhost:5173/${slug}`}
+//         className="w-full h-full"
+//         style={{ border: "none" }}
+//         title="Video Editor"
+//       />
+//     </div>
+//   );
+// };
+
+// export default Edit;
+
+import React, { useState, useEffect } from "react";
 import { useRouter } from "next/router";
+import Loader from "../../src/component/common/Loading/loading";
 
 const Edit = () => {
   const router = useRouter();
   const { slug } = router.query;
-  console.log(slug);
+
+  const [loading, setLoading] = useState(true);
+
+  // optional: wait for slug to be available
+  useEffect(() => {
+    if (slug) {
+      setLoading(true);
+    }
+  }, [slug]);
+
   return (
-    <div className="h-screen w-screen">
-      <iframe
-        src={`http://localhost:5173/${slug}`}
-        className="w-full h-full"
-        style={{ border: "none" }}
-        title="Video Editor"
-      />
+    <div className="h-screen w-screen relative">
+      {loading && <Loader />}
+      {slug && (
+        <iframe
+          src={`http://20.244.29.252:5173/${slug}`}
+          // src={`http://localhost:5173/${slug}`}
+          className="w-full h-full"
+          style={{ border: "none" }}
+          title="Video Editor"
+          onLoad={() => setLoading(false)}
+        />
+      )}
     </div>
   );
 };
