ce0d792c58444076c647f6cb07304c442ac413ad
Prashan Agarwal <110465560+prashan-ft@users.noreply.github.com>
2025-05-17 15:06:47 +0400

    changed to 3 days

--- pages/subscription/index.js
+++ pages/subscription/index.js
@@ -51,7 +51,7 @@ const PricingPlans = () => {
     const updateCountdown = () => {
       const now = new Date();
       const startDate = new Date(userSubscriptionData.startDate);
-      const promoEndsAt = new Date((startDate.getTime()) + 37 * 24 * 60 * 60 * 1000);
+      const promoEndsAt = new Date((startDate.getTime()) + 3 * 24 * 60 * 60 * 1000);
 
       const diff = promoEndsAt - now;
       
