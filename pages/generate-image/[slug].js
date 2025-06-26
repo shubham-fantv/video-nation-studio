aad84bebaaf01d5f76ffd0b38d83b7c1fbcd09af
Ayush <ayushawasthi2409@gmail.com>
2025-06-26 11:31:28 +0530

    useRef defined

--- pages/generate-image/[slug].js
+++ pages/generate-image/[slug].js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import AvatarDropdown from "../../src/component/common/AvatarDropdown";
 import { useRouter } from "next/router";
 import { useMutation, useQuery } from "react-query";
